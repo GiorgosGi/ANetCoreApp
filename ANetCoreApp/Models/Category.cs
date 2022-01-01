e3c875059e3a43cf1969dbcdc55c2c75f7df8f87
GiorgosGi <73617921+GiorgosGi@users.noreply.github.com>
2022-01-01 20:03:33 +0200

    Update Category.cs

--- ANetCoreApp/Models/Category.cs
+++ ANetCoreApp/Models/Category.cs
@@ -10,7 +10,6 @@ namespace ANetCoreApp.Models
     {
         [Key]
         public int Id { get; set; }
-
         public string Name { get; set; }
         public int DisplayOrder { get; set; }
     }
